77869d41879c80be0a359d407538d35cda788097
sayachan <sayachan@sayanombookpuro>
2020-03-10 21:05:22 +0900

    「修正」山札の進行状況を記録する変数deckCountを定義

--- BlackJack/src/blackjack/Deck.java
+++ BlackJack/src/blackjack/Deck.java
@@ -4,8 +4,9 @@ import java.util.Collections;
 import java.util.List;
 
 public class Deck {
-	
-	
+
+	int deckCount; //山札の進行状況を記録する変数deckCountを定義
+
 	// 山札（deck）に値を入れ、シャッフルするメソッド
 	public void shuffleDeck(List<Integer> deck) {
 		// リストに1-52の連番を代入
