bb91e94f8f0ea788ad25723fe8b19d8b4de5953f
sayachan <sayachan@sayanombookpuro>
2020-03-12 21:25:24 +0900

    playerをDeckのサブクラスに

--- BlackJack/src/blackjack/Player.java
+++ BlackJack/src/blackjack/Player.java
@@ -3,7 +3,7 @@ package blackjack;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Player {
+public class Player extends Deck {
 
 	List<Integer> player = new ArrayList<>(); //プレイヤーの手札リストを作成
 
